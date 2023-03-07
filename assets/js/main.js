4bb81dbe658e1c2584f2faf3c43c36eb9711cbe4
Victoria <121200782+odinaeva-vika@users.noreply.github.com>
2023-03-07 15:57:03 +0300

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,4 @@
-/* бургер + скролл*/
-
+  /*бургер + скрол по секциям*/
 const burger = document?.querySelector("[data-burger]");
 const menu = document?.querySelector("[data-nav]");
 const menuItems = menu.querySelectorAll("a");
@@ -16,10 +15,25 @@ menuItems.forEach(el => {
   el.addEventListener("click", () => {
     body.classList.remove("stop-scroll");
     menu?.classList.remove("menu--visible");
-    burger?.classList.remove("menu-icon--active");
   });
 });
 
 icon?.addEventListener("click", () => {
   burger?.classList.toggle("menu-icon--active");
 });
+
+/*скрол по разделам - убираем перелинковку в адресной строке*/
+
+  const smoothLinks = document.querySelectorAll('a[href^="#"]');
+
+  for (let smoothLink of smoothLinks) {
+      smoothLink.addEventListener('click', function (e) {
+          e.preventDefault();
+          const id = smoothLink.getAttribute('href');
+  
+          document.querySelector(id).scrollIntoView({
+              behavior: 'smooth',
+              block: 'start'
+          });
+      });
+  };
