b4d439db1d4ecab0bef42ba5cce496864c3fe089
Victoria <121200782+odinaeva-vika@users.noreply.github.com>
2023-03-18 04:09:57 +0300

    init

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,39 +1,18 @@
-  /*бургер + скрол по секциям*/
-const burger = document?.querySelector("[data-burger]");
-const menu = document?.querySelector("[data-nav]");
-const menuItems = menu.querySelectorAll("a");
-const icon = document?.querySelector(".menu-icon");
-
+const burger = document?.querySelector('[data-burger]');
+const menu = document?.querySelector('[data-nav]');
+const menuItems = menu?.querySelectorAll('a');
 const body = document.body;
 
-burger?.addEventListener("click", () => {
-  body.classList.toggle("stop-scroll");
-  menu?.classList.toggle("menu--visible");
-});
-
-menuItems.forEach(el => {
-  el.addEventListener("click", () => {
-    body.classList.remove("stop-scroll");
-    menu?.classList.remove("menu--visible");
+burger?.addEventListener('click', ()=> {
+    body.classList.toggle('stop-scroll');
+    burger?.classList.toggle('burger--active');
+    menu?.classList.toggle('menu--visible');
   });
-});
 
-icon?.addEventListener("click", () => {
-  burger?.classList.toggle("menu-icon--active");
-});
-
-/*скрол по разделам - убираем перелинковку в адресной строке*/
-
-  const smoothLinks = document.querySelectorAll('a[href^="#"]');
-
-  for (let smoothLink of smoothLinks) {
-      smoothLink.addEventListener('click', function (e) {
-          e.preventDefault();
-          const id = smoothLink.getAttribute('href');
-  
-          document.querySelector(id).scrollIntoView({
-              behavior: 'smooth',
-              block: 'start'
-          });
+menuItems.forEach(el=> {
+    el.addEventListener('click', ()=> {
+        body.classList.remove('stop-scroll');
+        burger?.classList.remove('burger--active');
+        menu?.classList.remove('menu--visible');
       });
-  };
+  });
